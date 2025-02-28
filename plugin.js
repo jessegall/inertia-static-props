6f55207a2fa6f64098d17be7c29572efcda74f2a
Jesse Gall <jesse-gall@hotmail.com>
2025-02-28 15:13:12 +0100

    Added plugin

---
+++ plugin.js
@@ -0,0 +1,51 @@
+import {usePage} from "@inertiajs/vue3";
+import {nextTick} from "vue";
+import axios from "axios";
+
+export const inertiaStaticPropsPlugin = {
+    install() {
+        const staticProps = {};
+
+        nextTick(() => {
+            const page = usePage();
+
+            if (page.props.staticProps) {
+                loadStaticProps(page.props.staticProps, page.props);
+            }
+        })
+
+        axios.interceptors.response.use(response => {
+            if (shouldInjectStaticProps(response)) {
+                injectStaticProps(response);
+            }
+
+            return response;
+        });
+
+        function loadStaticProps(keys, props) {
+            for (const key of keys) {
+                staticProps[key] = props[key];
+            }
+        }
+
+        function shouldInjectStaticProps(response) {
+            return response.headers['x-inertia'] && !response.config.headers['X-Inertia-Partial-Data'];
+        }
+
+        function injectStaticProps(response) {
+            const data = JSON.parse(response.data);
+
+            if (data.props.staticProps) {
+                loadStaticProps(data.props.staticProps, data.props);
+            }
+
+            for (const key in staticProps) {
+                data.props[key] = staticProps[key];
+            }
+
+            response.data = JSON.stringify(data);
+        }
+    }
+}
+
+export default inertiaStaticPropsPlugin;
